f351114319f8f30001a21fa82b985e3eac70d5d1
Tanya <thetwtanya@gmail.com>
2022-05-22 11:47:58 +0800

    add計時

--- de.py
+++ de.py
@@ -1,20 +1,39 @@
 # decorator 裝飾器
+import time
+
 def print_func(func):
     def inner():
         print('running', func.__name__)  # 拿__name__的屬性
         func()
     return inner   # 把function return出來 -> 成為最後一行的test
 
+def time_func(func):
+    def inner():
+        start = time.time()
+        func()
+        end = time.time()
+        print('elapsed', end - start, 'seconds')
+    return inner
+
+
 
 def test():
-    print('hi')
+    for i in range(10000000):
+        i = i + 1
 
 def test2():
     print('ni hao')
 
-test()
-print('加工中')
-test = print_func(test)
-test2 = print_func(test2)
+# test()
+# print('加工中')
+test = time_func(test)
+test2 = time_func(test2)
 test()
 test2()
+
+# import time
+
+# start = time.time()
+# test()
+# end = time.time()
+# print('elapsed', end - start, 'seconds')
